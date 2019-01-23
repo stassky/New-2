47e55eb6279efb526f301712999da57664e6058b
Stas <stasyam@gmail.com>
2019-01-23 21:33:15 +0200

    adding selenium test

---
+++ final_project.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+import time
+
+
+driver = webdriver.Firefox(executable_path="D:\Selenium\geckodriver.exe")
+driver.get("http://192.168.99.100:5000/")
+driver.implicitly_wait(10)
+
+
+
+# FIND ELEMENT AND PRINT IT (READ)
