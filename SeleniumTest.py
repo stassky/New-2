e5299b9011097da500fdeacd198f6a7076a26f66
Stas <stasyam@gmail.com>
2019-01-25 20:24:57 +0200

    / removed from address

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -4,7 +4,7 @@ import time
 
 
 driver = webdriver.Firefox(executable_path="D:\Selenium\geckodriver.exe")
-driver.get("http://192.168.99.100:5000/")
+driver.get("http://192.168.99.100:5000")
 driver.implicitly_wait(10)
 
 
